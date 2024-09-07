f61ffd50046f1094ac1ff9e28277cda161ff5e66
sankalproy11 <sankalproy.dev@gmail.com>
2024-09-07 23:31:27 +0530

    parsed header of the dns packet

--- src/server.js
+++ src/server.js
@@ -1,10 +1,28 @@
 const dgram = require("dgram");
 const server = dgram.createSocket("udp4");
 
-server.on("message", (msg) => {
-  console.log("Received Buffer: ", msg);
-  console.log("Buffer length: ", msg.length);
-  console.log("Buffer As string: ", msg.toString("utf-8"));
+server.on("message", (msg, rinfo) => {
+  console.log(`Received packet from ${rinfo.address}:${rinfo.port}`);
+  console.log(`Received raw buffer from: `, msg);
+  console.log(`Buffer Length: `, msg.length);
+
+  try {
+    const transactionId = msg.readUint16BE(0);
+    const flags = msg.readUint16BE(2);
+    const questionCount = msg.readUInt16BE(4);
+    const answerCount = msg.readUInt16BE(6);
+    const authorityCount = msg.readUInt16BE(8);
+    const additionalCount = msg.readUInt16BE(10);
+
+    console.log(`Transaction ID: ${transactionId}`);
+    console.log(`Flags: ${flags}`);
+    console.log(`Question Count: ${questionCount}`);
+    console.log(`Answer Count: ${answerCount}`);
+    console.log(`Authority Count: ${authorityCount}`);
+    console.log(`Additional Count: ${additionalCount}`);
+  } catch (error) {
+    console.error("Error parsing DNS packet:", error.message);
+  }
 });
 
 server.bind(8080, () => {
